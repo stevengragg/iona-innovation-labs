03ebe3bf6437f105568aeb807a4337386ef09846
Steven Gragg <sgragg5619@gmail.com>
2025-04-11 14:49:23 +0800

    ref: blog page content, update footer links, and enhance global styles

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,8 +8,10 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
-      <BlogPosts />
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+        Here we will share usefull information for everyone.
+      </h1>
+      {/* <BlogPosts /> */}
     </section>
   );
 }
